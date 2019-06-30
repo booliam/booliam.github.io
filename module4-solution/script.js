8ef36df0207dafc33054eee0992b4e82868cde29
booliam <liamrbrake@penguin>
2019-06-30 11:19:23 -0400

    finished

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -51,19 +51,16 @@ WARNING!!! WARNING!!!
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   // var firstLetter =
-    var firstLetter = function () {
-      names.charAt(0);
-      names.toLowerCase();
-    }
+    var firstLetter = names[i].charAt(0).toLowerCase();
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
     if (firstLetter == 'j') {
-      byeSpeaker.speak;
+      byeSpeaker.speak(names[i]);
     } else {
-      helloSpeaker.speak;
+      helloSpeaker.speak(names[i]);
     }
   }
 
