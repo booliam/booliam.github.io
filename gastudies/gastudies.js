16b04dd69fc4f365ded8c05637b5b5817438f5ec
booliam <37272385+booliam@users.noreply.github.com>
2019-12-09 15:01:44 -0500

    added js

--- gastudies/gastudies.js
+++ gastudies/gastudies.js
@@ -1,1 +1,23 @@
+function getPlayers() {
+var firstPlayer = prompt("Enter first player's name.");
+var secondPlayer = prompt("Enter second player's name.");
+//Asks if another player wants to be added.
+var thirdPlayerConfirm = prompt("Would you like a third player to be added? YES if Yes. NO if No.");
+if (thirdPlayerConfirm == "YES") {
+  var thirdPlayer = prompt("Enter third player's name.");
+} else {
+  alert("Ok.");
+}
+var returntoStartScreen = prompt("To start the game, type START. To return to character entering screen, type BACK.");
+ if (returntoStartScreen == BACK) {
+   getPlayers();
+ } else {
+   beginGame();
+ }
+}
+ 
+function beginGame() {
+ alert("Game started");
+}
+
 
