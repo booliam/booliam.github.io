6b9e7815f9b8cee625dca98dcd94e00a8be015eb
booliam <liamrbrake@gmail.com>
2019-12-09 20:05:46 -0500

    Update gastudies.js

--- gastudies/gastudies.js
+++ gastudies/gastudies.js
@@ -2,22 +2,20 @@ function getPlayers() {
 var firstPlayer = prompt("Enter first player's name.");
 var secondPlayer = prompt("Enter second player's name.");
 //Asks if another player wants to be added.
-var thirdPlayerConfirm = prompt("Would you like a third player to be added? YES if Yes. NO if No.");
-if (thirdPlayerConfirm == "YES") {
+var thirdPlayerConfirm = prompt("Would you like a third player to be added? Type 'yes' if yes, 'no' if no.");
+if (thirdPlayerConfirm == "yes") {
   var thirdPlayer = prompt("Enter third player's name.");
 } else {
-  alert("Ok.");
+  alert("Continue.");
 }
-var returntoStartScreen = prompt("To start the game, type START. To return to character entering screen, type BACK.");
- if (returntoStartScreen == BACK) {
+var returntoStartScreen = prompt("To start the game, type 'start'. To return to character entering screen, type 'back'.");
+ if (returntoStartScreen == "back") {
    getPlayers();
  } else {
    beginGame();
  }
 }
- 
+
 function beginGame() {
- alert("Game started");
+ alert("Game will begin");
 }
-
-
