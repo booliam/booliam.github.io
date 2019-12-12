080cd33c0fcefb9893bea068f2046486edb9a8d1
booliam <liamrbrake@gmail.com>
2019-12-12 16:49:16 -0500

    Create login.js

---
+++ gastudies/login.js
@@ -0,0 +1,28 @@
+function auth(form) {
+  var user = form.username.value;
+  var pass = form.pwd.value;
+
+  var admin = 'booliam';
+  var adminpwd = 'Lanpher9399';
+
+  var admin2 = 'evan';
+  var admin2pwd = '072305';
+
+  var admin3 = 'petergriffin';
+  var admin3pwd = 'shalmik23';
+
+  var admin4 = 'yeehawdaddy.edu';
+  var admin4pwd = 'guitarpreston52';
+
+  if (user == admin && pass == adminpwd) {
+    window.open('admin.html', '_self');
+  } else if (user == admin2 && pass == admin2pwd) {
+    window.open('admin.html', '_self');
+  } else if (user == admin3 && pass == admin3pwd) {
+    window.open('admin.html', '_self');
+  } else if (user == admin4 && pass == admin4pwd) {
+    window.open('admin.html', '_self');
+  } else {
+    window.open('gastudies.html', '_self');
+  }
+}
